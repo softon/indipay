5d4b961afc9a9eb65a92457d70179dcb766f552a
Shiburaj <powerupneo@gmail.com>
2016-07-18 22:01:42 +0530

    Solved Issue #12

--- src/Gateways/EBSGateway.php
+++ src/Gateways/EBSGateway.php
@@ -115,7 +115,7 @@ class EBSGateway implements PaymentGatewayInterface {
     protected function encrypt()
     {
         $this->hash = '';
-        $hash_string = "ebskey"."|".urlencode($this->parameters['account_id'])."|".urlencode($this->parameters['amount'])."|".urlencode($this->parameters['reference_no'])."|".$this->parameters['return_url']."|".urlencode($this->parameters['mode']);
+        $hash_string = $this->secretKey."|".urlencode($this->parameters['account_id'])."|".urlencode($this->parameters['amount'])."|".urlencode($this->parameters['reference_no'])."|".$this->parameters['return_url']."|".urlencode($this->parameters['mode']);
         $this->hash = md5($hash_string);
     }
 
